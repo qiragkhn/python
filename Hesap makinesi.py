9c34d0dcc815afbea45213f7c0a2b17a7e8bd98d
qiragkhn <61393047+qiragkhn@users.noreply.github.com>
2020-03-30 12:57:00 +0300

    Add files via upload

---
+++ Hesap makinesi.py
@@ -0,0 +1,12 @@
+print(""" ****************************************
+HESAP Makinesi Programı 
+
+İşlemler: 
+
+1. Toplama işlemi 
+2. Çıkarma işlemi 
+3. Çarpma işlemi
+4. Bölme işlemi 
+
+*****************************************
+""")
